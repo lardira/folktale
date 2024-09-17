b3324e13bd5cecf51e7172271bcbfdcd8c8df983
lardira <78199923+lardira@users.noreply.github.com>
2024-09-17 13:40:09 +0300

    enable cors (#18)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
-async function bootstrap() {
+(async () => {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  app.enableCors();
+
   await app.listen(3000);
-}
-bootstrap();
+})();
