5047948401fdb15b31a7a0e2e4d07e4287992c2b
lardira <danilkefimov@gmail.com>
2024-09-19 14:33:25 +0300

    feature creating new chapter

--- frontend/src/lib/debug.js
+++ frontend/src/lib/debug.js
@@ -1,3 +1,7 @@
 export const tale = {
 	img: 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg'
 };
+
+export const user = {
+	id: 'ec447dbb-d030-483e-afb2-6c53b18e83be'
+}
--- frontend/src/routes/tales/[slug]/+page.svelte
+++ frontend/src/routes/tales/[slug]/+page.svelte
@@ -1,8 +1,7 @@
 <script>
 	import * as debug from '$lib/debug.js';
-	import { config } from '$lib/utils.js';
-	import axios from 'axios';
 	import ChapterView from '../../../components/ChapterView.svelte';
+	import Divider from '../../../components/Divider.svelte';
 
 	export let data;
 
@@ -15,7 +14,7 @@
 	<div class="tale-view">
 		<div class="tale-info-container flex flex-row space-x-4">
 			<div class="tale-image">
-				<img class="h-auto max-w-md" src={debug.tale.img} alt={tale.title} />
+				<img class="h-auto max-w-sm" src={debug.tale.img} alt={tale.title} />
 			</div>
 			<div class="tale-info flex-column">
 				<h2 class="h2">{tale.title}</h2>
@@ -30,10 +29,9 @@
 				</span>
 			</div>
 		</div>
-
+		<Divider />
 		<div class="chapters-view-container">
 			<div class="chapters-view">
-				<h3 class="chapters-view-title">Chapters</h3>
 				<ul>
 					<li>
 						{#each chapters as chapter, i (chapter.id)}
